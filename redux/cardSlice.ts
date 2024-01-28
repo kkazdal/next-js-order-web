074836d6d1a9f857a767c6b1e890da8fddb0ac5a
Kadir Kazdal <>
2024-01-28 18:18:23 +0300

    feat: User modeli eklendi.

--- redux/cardSlice.ts
+++ redux/cardSlice.ts
@@ -13,8 +13,13 @@ const cardSlice: any = createSlice({
             state.quantity += action.payload.quantity;
             state.total += action.payload.price;
         },
+        reset: (state, action) => {
+            state.products = [];
+            state.quantity = 0;
+            state.total = 0;
+        },
     },
 });
 
-export const { addProduct } = cardSlice.actions;
+export const { addProduct, reset } = cardSlice.actions;
 export default cardSlice.reducer;
