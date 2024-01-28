7ac5f6da5c031fcd0d634aa69c955caabfd4c386
Kadir Kazdal <>
2024-01-28 21:10:01 +0300

    feat: mongo adapter eklendi.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import GithubProvider from "next-auth/providers/github";
+import clientPromise from "@/util/mongo";
 
 //Github,google,yandex gibi ikinci parti yazılımlarından register işlemi yapmak için kullan.
 export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
     // Configure one or more authentication providers
     providers: [
         GithubProvider({
@@ -10,4 +13,7 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_SECRET ?? "",
         }),
     ],
+    pages: {
+        signIn: "/auth/login"
+    }
 });
